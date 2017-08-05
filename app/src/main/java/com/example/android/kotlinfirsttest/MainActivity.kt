1fba3ce0dc78e8f10b53507b410ed619fbc41de2
DaveKun <dawidkunicki88@gmail.com>
2017-08-05 18:27:14 +0200

    All necessary dependencies set up. Kotlin magic works

--- app/src/main/java/com/example/android/kotlinfirsttest/MainActivity.kt
+++ app/src/main/java/com/example/android/kotlinfirsttest/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.example.android.kotlinfirsttest
 
+
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.find
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        message.text = "Hello Kotlin!"
+
     }
 }
