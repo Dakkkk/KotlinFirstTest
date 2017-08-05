8d2e0d7cf11f7e8dd3111adb879adadec7a161be
DaveKun <dawidkunicki88@gmail.com>
2017-08-05 18:52:09 +0200

    Added RecyclerView dependency and initialized it

--- app/src/main/java/com/example/android/kotlinfirsttest/MainActivity.kt
+++ app/src/main/java/com/example/android/kotlinfirsttest/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.android.kotlinfirsttest
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.find
 class MainActivity : AppCompatActivity() {
@@ -10,7 +12,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        message.text = "Hello Kotlin!"
+        val forecastList = findViewById(R.id.forecast_list) as RecyclerView
+        forecastList.layoutManager = LinearLayoutManager(this)
 
     }
 }
