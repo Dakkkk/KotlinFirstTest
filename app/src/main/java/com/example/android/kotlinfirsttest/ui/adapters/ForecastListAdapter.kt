bd214ea5bf74e66d7040cbff9b872671a64c1276
DaveKun <dawidkunicki88@gmail.com>
2017-08-09 15:02:48 +0200

    Added comment in adapter

--- app/src/main/java/com/example/android/kotlinfirsttest/ui/adapters/ForecastListAdapter.kt
+++ app/src/main/java/com/example/android/kotlinfirsttest/ui/adapters/ForecastListAdapter.kt
@@ -16,6 +16,13 @@ class ForecastListAdapter(val weekForecast: ForecastList) : RecyclerView.Adapter
         return ViewHolder(TextView(parent.context))
     }
 
+    /*'with' is a function included in the standard Kotlin library. It
+     basically receives an object and an extension function as parameters, and
+     makes the object execute the function. This means that all the code we
+     define inside the brackets acts as an extension function for the object
+     provided in the first parameter, and we can use all its public functions
+     and properties, as well as this. Helpful to simplify code when we
+     do several operations over the same object.*/
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         with(weekForecast.dailyForecast[position]) {
             holder.textView.text = "$date - $description - $high/$low"
