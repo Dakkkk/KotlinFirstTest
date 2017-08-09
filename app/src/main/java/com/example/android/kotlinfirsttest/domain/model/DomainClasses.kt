1953583799713f67f60b7aa718add260225e9c1f
DaveKun <dawidkunicki88@gmail.com>
2017-08-09 15:44:47 +0200

    Overloaded the get function of ForecastList class to get the dailyForecast[position] ( used in adapter) more easily

--- app/src/main/java/com/example/android/kotlinfirsttest/domain/model/DomainClasses.kt
+++ app/src/main/java/com/example/android/kotlinfirsttest/domain/model/DomainClasses.kt
@@ -3,5 +3,13 @@ package com.example.android.kotlinfirsttest.domain.model
 /**
  * Created by Dawid on 2017-08-07.
  */
-data class ForecastList(val city: String, val country: String, val dailyForecast: List<Forecast>)
+data class ForecastList(val city: String, val country: String,
+                        val dailyForecast: List<Forecast>) {
+    val size: Int
+        get() = dailyForecast.size
+
+    /*We use operator modifier to overload the get function, so we can get the dailyForecast[position mokre easily]*/
+    operator fun get(position: Int): Forecast = dailyForecast[position]
+}
+
 data class Forecast(val date: String, val description: String, val high: Int, val low: Int)
