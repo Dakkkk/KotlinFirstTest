039c38eadeaf10cf69d3d34a0b8d3942ee268484
DaveKun <dawidkunicki88@gmail.com>
2017-08-09 14:55:17 +0200

    Changed the MainActivity and ForecastListAdapter classes to use the RequestForecastCommand  and ForecastList classes to fetch the real data from the API

--- app/src/main/java/com/example/android/kotlinfirsttest/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/android/kotlinfirsttest/ui/activities/MainActivity.kt
@@ -7,28 +7,27 @@ import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import com.example.android.kotlinfirsttest.adapters.ForecastListAdapter
 import com.example.android.kotlinfirsttest.R
+import com.example.android.kotlinfirsttest.domain.commands.RequestForecastCommand
+import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.find
+import org.jetbrains.anko.uiThread
 
 class MainActivity : AppCompatActivity() {
-
-    private val items = listOf(
-            "Mon 6/23 - Sunny - 31/17",
-            "Tue 6/24 - Foggy - 21/8",
-            "Wed 6/25 - Cloudy - 22/17",
-            "Thurs 6/26 - Rainy - 18/11",
-            "Fri 6/27 - Foggy - 21/10",
-            "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
-            "Sun 6/29 - Sunny - 20/7"
-    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        /* using Kotlins ANKO find extension function */
-        val forecastList: RecyclerView = find(R.id.forecast_list)
-        //val forecastList = findViewById(R.id.forecast_list) as RecyclerView
-        forecastList.layoutManager = LinearLayoutManager(this)
-        forecastList.adapter = ForecastListAdapter(items)
 
+        val forecastList: RecyclerView = find(R.id.forecast_list)
+        forecastList.layoutManager = LinearLayoutManager(this)
+
+        doAsync {
+            val result = RequestForecastCommand("94043").execute()
+            uiThread {
+                forecastList.adapter = ForecastListAdapter(result)
+            }
+        }
     }
+
 }
+
--- app/src/main/java/com/example/android/kotlinfirsttest/ui/adapters/ForecastListAdapter.kt
+++ app/src/main/java/com/example/android/kotlinfirsttest/ui/adapters/ForecastListAdapter.kt
@@ -3,12 +3,13 @@ package com.example.android.kotlinfirsttest.adapters
 import android.support.v7.widget.RecyclerView
 import android.view.ViewGroup
 import android.widget.TextView
+import com.example.android.kotlinfirsttest.domain.model.ForecastList
 
 /**
  * Created by Dawid on 2017-08-05.
  */
-class ForecastListAdapter(val items: List<String>) :
-        RecyclerView.Adapter<ForecastListAdapter.ViewHolder>() {
+class ForecastListAdapter(val weekForecast: ForecastList) : RecyclerView.Adapter<ForecastListAdapter.ViewHolder>() {
+
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int):
             ViewHolder {
@@ -16,10 +17,12 @@ class ForecastListAdapter(val items: List<String>) :
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.textView.text = items[position]
+        with(weekForecast.dailyForecast[position]) {
+            holder.textView.text = "$date - $description - $high/$low"
+        }
     }
 
-    override fun getItemCount(): Int = items.size
+    override fun getItemCount(): Int = weekForecast.dailyForecast.size
 
     class ViewHolder(val textView: TextView) : RecyclerView.ViewHolder(textView)
 }
