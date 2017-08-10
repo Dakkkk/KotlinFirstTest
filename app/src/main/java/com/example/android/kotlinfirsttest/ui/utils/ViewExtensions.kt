441f586864f7e36fb5f0972e5c5ccfa4dc481ce1
DaveKun <dawidkunicki88@gmail.com>
2017-08-10 14:33:11 +0200

    Modified the adapter to serve onClicks

---
+++ app/src/main/java/com/example/android/kotlinfirsttest/ui/utils/ViewExtensions.kt
@@ -0,0 +1,10 @@
+package com.example.android.kotlinfirsttest.ui.utils
+
+/**
+ * Created by Dawid on 2017-08-10.
+ */
+import android.content.Context
+import android.view.View
+
+val View.ctx: Context
+    get() = context
